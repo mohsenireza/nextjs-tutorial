271576ac08621aba931ed54dd314109e9cfbb72c
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2025-07-15 22:17:41 +0330

    Add authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { auth, signIn, signOut } from '@/auth';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = async () => {
+    const session = await auth();
+
+    return (
+        <header className="px-5 py-3 shadow-sm bg-white font-work-sans">
+            <nav className="flex items-center justify-between">
+                <Link href="/">
+                    <Image src="/next.svg" alt="logo" width={144} height={20} />
+                </Link>
+
+                <div className="flex items-center gap-4 text-black">
+                    {session && session.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                Create
+                            </Link>
+
+                            <form action={async () => {
+                                "use server";
+
+                                await signOut({redirectTo:"/"});
+                            }}>
+                                 <button type="submit">
+                                    Logout
+                                </button>
+                            </form>
+
+                            <Link href={`/user/${session.user.id}`}>
+                                <span>{session.user.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <form action={async () => {
+                            "use server";
+
+                            await signIn('github')
+                        }}>
+                            <button type="submit">
+                                Login
+                            </button>
+                        </form>
+                    )}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
