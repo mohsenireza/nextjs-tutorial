48a7843cf60faab35ea1c7a96d55a6b0a769e6ad
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2025-08-08 21:45:17 +0330

    Create author in Sanity when user logs in

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
@@ -8,6 +9,10 @@ export default async function Home({ searchParams } : { searchParams: Promise<{ 
   const query = (await searchParams).query;
   const params = { search: query || null };
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+  const session = await auth();
+
+  console.log(session?.id);
 
   return (
     <>
